5536102eeea5b90f69a5709761ee17fcfe0fea59
Wind-Ward <xuan223344@126.com>
2017-07-03 13:20:29 +0800

    3th commit

---
+++ SAG.py
@@ -0,0 +1,22 @@
+
+class Vertice(object):
+    def __init__(self):
+        self.S=[]
+        self.time=None
+class Edge(object):
+    def __init__(self):
+        self.x=None
+        self.y=None
+        self.w=
+
+class SAGModel(object):
+
+    def __init__(self):
+        pass
+
+    def _tag_extraction(self):
+
+
+
+
+if __name__ == '__main__':
